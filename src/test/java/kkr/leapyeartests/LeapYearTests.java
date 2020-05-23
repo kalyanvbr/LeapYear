cd3141c16a5a1632aec705388e75eee0e40ef9cd
Kalyan Reddy <kalyanvbr@gmail.com>
2020-05-24 02:09:38 +0530

    add test to verify that the year divisible by 100 but not by 400 is not a leap year

--- src/test/java/kkr/leapyeartests/LeapYearTests.java
+++ src/test/java/kkr/leapyeartests/LeapYearTests.java
@@ -12,7 +12,7 @@ public class LeapYearTests {
 	private int yearDivisibleByFourHundred = 2000;
 	private int yearDivisibleByFourButNotByOneHundred = 2016;
 	private int yearNotDivisibleByFour = 2019;
-	private int yearDivisibleByOneHundredButNotByFourHundred = 1700;
+	private int yearDivisibleByOneHundredButNotByFourHundred = 1800;
 
 	@Before
 	public void setUp() throws Exception {
@@ -20,20 +20,26 @@ public class LeapYearTests {
 	}
 	
 	@Test
-	public void yearsDivisibleByFourButNotByHundredAreLeapYears() {
+	public void yearDivisibleByFourButNotByHundredAreLeapYears() {
 		assertEquals(yearDivisibleByFourButNotByOneHundred + " is a leap year", 
 				leapYear.isLeapYear(yearDivisibleByFourButNotByOneHundred));
 	}
 	
 	@Test
-	public void yearsNotDivisbleByFourAreNotLeapYears() {
+	public void yearNotDivisbleByFourAreNotLeapYears() {
 		assertEquals(yearNotDivisibleByFour + " is not a leap year", 
 				leapYear.isLeapYear(yearNotDivisibleByFour));
 	}
 	
 	@Test
-	public void yearsDivisibleByFourHundredAreLeapYears() {
+	public void yearDivisibleByFourHundredAreLeapYears() {
 		assertEquals(yearDivisibleByFourHundred + " is a leap year", 
 				leapYear.isLeapYear(yearDivisibleByFourHundred));
+	}
+	
+	@Test
+	public void yearDivisibleByOneHundredButNotByFourHundredAreNotLeapYears() {
+		assertEquals(yearDivisibleByOneHundredButNotByFourHundred + " is not a leap year", 
+				leapYear.isLeapYear(yearDivisibleByOneHundredButNotByFourHundred));
 	}
 }
